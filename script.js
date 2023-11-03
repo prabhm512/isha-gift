853176543c46be7596ff0ad4945f9e85ddbf24c8
prabhm512 <prabhm512@gmail.com>
2023-11-03 17:17:29 +1100

    key updated

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const submitButton = document.getElementById("submitButton");
     const message = document.getElementById("message");
 
-    const key = "test"
+    // let keyPart1 = "t"
+    // let keyPart2 = "e"
+    // let keyPart3 = "st"
 
     const tryAgainMessages = [
         "Oops! Try again!",
@@ -13,13 +15,29 @@ document.addEventListener("DOMContentLoaded", function () {
       ];
 
     submitButton.addEventListener("click", function () {
-        console.log("submit button working")
-        const enteredValue = valueInput.value;
+        const enteredValue = valueInput.value.toLowerCase();
+        console.log(enteredValue)
 
         // Check if the entered value is correct
-        if (enteredValue === key) {
-            // If correct, navigate to the desired link
-            window.location.href = "https://drive.google.com/file/d/18pT_Sw7SfAtOg2yl5Rmkz_NjFHUgku7d/view?usp=sharing";
+        if (enteredValue === '2022-10-24') {
+
+            // Get a reference to the body element
+            var body = document.getElementsByClassName("container");
+            // Remove all child elements from the body
+            while (body.firstChild) {
+                body.removeChild(body.firstChild);
+            }
+
+            // add image
+            var img = document.createElement("img");
+            img.src = "./Diwali 2022.jpeg";
+
+            document.body.appendChild(img);
+
+            setTimeout(function() {
+                window.location.href = "https://drive.google.com/file/d/18pT_Sw7SfAtOg2yl5Rmkz_NjFHUgku7d/view?usp=sharing"; // Replace with your desired link.
+            }, 15000);
+
         } else {
             // If incorrect, display an error message
             // message.innerText = "Try again!";
